0c4d51a276299b3c40c35c5b430fb721cfb3fa97
Ender Bakır <enderyx_123@hotmail.com>
2024-02-05 16:15:41 +0300

    Initial Commit
    Models and responsedto classes were created for the Catalog Service.

---
+++ Shared/FreeCourse.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; private set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public List<string> Errors { get; set; }
+
+        public static ResponseDto<T> Success(T data, int statusCode)//for get operations and create operations
+        {
+            return new ResponseDto<T>
+            {
+                Data = data,
+                StatusCode = statusCode,
+                IsSuccessful = true
+            };
+        }
+
+        public static ResponseDto<T> Success(int statusCode)//for update and delete operations
+        {
+
+            return new ResponseDto<T>
+            {
+                Data = default(T),
+                StatusCode = statusCode,
+                IsSuccessful = true
+            };
+        }
+
+        public static ResponseDto<T>Fail(List<string> errors , int statusCode)//for get operations and create operations fail
+        {
+            return new ResponseDto<T>
+            {
+                Errors = errors,
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+
+        public static ResponseDto<T>Fail(string error , int statusCode)//for update and delete operations fail
+        {
+            return new ResponseDto<T>
+            {
+                Errors = new List<string> { error },
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+    }
+}
