4f2c1cb838a4c7a8f572911449912da161e00edd
Ender Bakır <enderyx_123@hotmail.com>
2024-02-06 11:08:21 +0300

    Options Pattern And AppsettingJson.
    options pattern applied, connection created for mongodb database.

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,4 +1,6 @@
+using FreeCourse.Services.Catalog.Settings;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddControllers();
+
+//Options Pattern MongoDb
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));//here we are binding the configuration to the DatabaseSettings class for the options pattern.
+
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;//here we are getting the value of the DatabaseSettings class from the options pattern.we use getRequiredService because we want to get the value of the DatabaseSettings class.
+});
+//Options Pattern
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
