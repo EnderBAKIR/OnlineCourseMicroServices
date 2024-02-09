01a4353f00d4ba841ecc719bf006b9dfd1902ff4
Ender Bakır <enderyx_123@hotmail.com>
2024-02-09 17:47:26 +0300

    IdentityServer , SqlConnection
    Identity Server was installed, Identity Server was set up to be a token distribution system by keeping it separate from other APIs, SQL server connections were made, seed data was added.

---
+++ IdentityServer/FreeCouse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCouse.IdentityServer.Dtos;
+using FreeCouse.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCouse.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            { UserName = signUpDto.UserName, Email = signUpDto.Email,City=signUpDto.City };
+
+            var result =await _userManager.CreateAsync(user,signUpDto.Password);
+
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+
+
+    }
+}
