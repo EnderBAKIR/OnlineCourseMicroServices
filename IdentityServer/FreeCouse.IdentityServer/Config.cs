5d9676cf9f5d319ed5973d201d723ad820b73051
Ender Bakır <enderyx_123@hotmail.com>
2024-02-09 22:56:25 +0300

    Config , IdentityResources
    In the config file, the user's email, ID and profile information are definet in Identity Resources.Refreshtoken and access token settings have been made.

--- IdentityServer/FreeCouse.IdentityServer/Config.cs
+++ IdentityServer/FreeCouse.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace FreeCouse.IdentityServer
@@ -22,7 +23,10 @@ namespace FreeCouse.IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
-                
+                    new IdentityResources.Email(),
+                    new IdentityResources.OpenId(),
+                    new IdentityResources.Profile(),
+                    new IdentityResource(){Name="roles" , DisplayName="Roles" , Description="Kullanıcı Rolleri" ,UserClaims=new[]{"role"}},
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -44,6 +48,19 @@ namespace FreeCouse.IdentityServer
                  ClientSecrets={new Secret ("secret".Sha256())},
                  AllowedGrantTypes=GrantTypes.ClientCredentials,
                  AllowedScopes={"catalog_fullpermission" , "photo_stock_fullpermission" , IdentityServerConstants.LocalApi.ScopeName} 
+             },
+              new Client
+             {
+                 ClientName = "Asp.Net Core MVC",
+                 ClientId="WebMvcClientForUser",
+                 AllowOfflineAccess=true,
+                 ClientSecrets={new Secret ("secret".Sha256())},
+                 AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                 AllowedScopes={IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess , "roles"},
+                 AccessTokenLifetime=1*60*60,
+                 RefreshTokenExpiration=TokenExpiration.Absolute,
+                 AbsoluteRefreshTokenLifetime=(int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                 RefreshTokenUsage=TokenUsage.ReUse
              }
 
 
