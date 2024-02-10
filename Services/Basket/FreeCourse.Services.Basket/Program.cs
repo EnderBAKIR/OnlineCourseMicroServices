d38513079dc4d7dbeb010263d88b1937abea4085
Ender Bakır <enderyx_123@hotmail.com>
2024-02-10 17:38:20 +0300

    RedisService , IBasketService , BasketService
    RedisService class was created to connect with Redis, and BasketService was created to perform crud operations in the Basket model.

--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,8 +1,22 @@
+using FreeCourse.Services.Basket.Services;
 using FreeCourse.Services.Basket.Setting;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+    var redisSettings = sp.GetRequiredService<IOptions<RedisSetting>>().Value;
+
+    var redis = new RedisService(redisSettings.Host , redisSettings.Port);
+
+    redis.Connect();
+    return redis;
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Services/Basket/FreeCourse.Services.Basket/Services/BasketService.cs
@@ -0,0 +1,44 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using System.Text.Json;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task<Response<bool>> Delete(string userId)
+        {
+            var status = await _redisService.GetDb().KeyDeleteAsync(userId);
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("bu kullanıcıya ait sepet bulunamadı", 404);
+        }
+
+        public async Task<Response<BasketDto>> GetBasket(string userId)
+        {
+            var existBasket= await _redisService.GetDb().StringGetAsync(userId);
+
+
+            if (String.IsNullOrEmpty(existBasket))
+            {
+                return Response<BasketDto>.Fail("bu kullanıcıya ait sepet bulunamadı", 404);
+            }
+
+            return Response<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(existBasket), 200);
+
+
+        }
+
+        public async Task<Response<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var status = await _redisService.GetDb().StringSetAsync(basketDto.UserId, JsonSerializer.Serialize(basketDto));
+
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Sepetiniz kayıt edilemedi veya güncellenemedi daha sonra tekrar deneyiniz", 500);
+        }
+    }
+}
