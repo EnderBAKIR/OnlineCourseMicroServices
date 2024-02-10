fa9416a5d37ac233a189a4f738d949e48b21074f
Ender Bakır <enderyx_123@hotmail.com>
2024-02-10 15:11:54 +0300

    Options Pattern And AppsettingJson.
    Options pattern applied, connection created for Redis.

--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,3 +1,5 @@
+using FreeCourse.Services.Basket.Setting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//options pattern
+builder.Services.Configure<RedisSetting>(builder.Configuration.GetSection("RedisSettings"));
+//options pattern
+
 
 var app = builder.Build();
 
---
+++ Services/Basket/FreeCourse.Services.Basket/Setting/RedisSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Services.Basket.Setting
+{
+    public class RedisSetting
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
