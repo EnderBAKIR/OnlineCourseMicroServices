73f7e679c8cd1c01d19dce6579243343aa58b472
Ender Bakır <enderyx_123@hotmail.com>
2024-02-11 15:13:19 +0300

    DiscountService , Docker , PostgreSql
    Crud methods were created with dapper for PostgreSql in DiscountService.

---
+++ Services/Discount/FreeCourse.Services.Discount/Services/DiscountService.cs
@@ -0,0 +1,92 @@
+﻿using Dapper;
+using FreeCourse.Shared.Dtos;
+using Npgsql;
+using System.Data;
+
+namespace FreeCourse.Services.Discount.Services
+{
+
+    public class DiscountService : IDiscountServices
+    {
+
+
+        private readonly IConfiguration _configuration;
+
+        private readonly IDbConnection _connection;
+
+        public DiscountService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            _connection = new NpgsqlConnection(_configuration.GetConnectionString("PostgreSql"));
+        }
+
+        public async Task<Response<NoContent>> Delete(int id)
+        {
+            var status = await _connection.ExecuteAsync("delete from discount where id=@Id", new { Id = id });
+
+            return status > 0 ? Response<NoContent>.Success(204) : Response<NoContent>.Fail("Kupon bulunamadı", 404);
+        }
+
+        public async Task<Response<List<Models.Discount>>> GetAll()
+        {
+            var discount = await _connection.QueryAsync<Models.Discount>("Select*from discount");
+
+            return Response<List<Models.Discount>>.Success(discount.ToList(), 200);
+        }
+
+        public async Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId)
+        {
+            var discount = await _connection.QueryAsync<Models.Discount>("select * from discount where userid=@UserId and code=@Code", new { UserId = userId, Code = code });
+            var hasDiscount = discount.FirstOrDefault();
+
+            if (hasDiscount == null)
+            {
+                return Response<Models.Discount>.Fail("Böyle bir kupon bulunumadı" , 404);
+            }
+            return Response<Models.Discount>.Success(hasDiscount, 200);
+        }
+
+        public async Task<Response<Models.Discount>> GetById(int id)
+        {
+            var discount = (await _connection.QueryAsync<Models.Discount>("select*from discount where id=@Id", new { Id = id })).SingleOrDefault();
+
+            if (discount == null)
+            {
+                return Response<Models.Discount>.Fail("Kupon Bulunamadı", 404);
+            }
+            return Response<Models.Discount>.Success(discount, 200);
+
+        }
+
+        public async Task<Response<NoContent>> Save(Models.Discount discount)
+        {
+            var saveStatus = await _connection.ExecuteAsync("INSERT INTO discount(userid,rate,code) VALUES(@UserId,@Rate,@Code)", discount);
+
+            if (saveStatus > 0)
+            {
+                return Response<NoContent>.Success(204);
+            }
+            return Response<NoContent>.Fail("Kupon oluşturulurken bir hata meydana geldi", 500);
+        }
+
+        public async Task<Response<NoContent>> Update(Models.Discount discount)
+        {
+            var status = await _connection.ExecuteAsync("update discount set userid=@UserId, code=@Code, rate=@Rate where İd=@Id", new
+            {
+                Id = discount.Id,
+                UserId = discount.UserId,
+                Code = discount.Code,
+                Rate = discount.Rate
+            });
+
+
+            if(status > 0)
+            {
+                return Response<NoContent>.Success(204);
+            }
+
+            return Response<NoContent>.Fail("Kupon bulunamadı" , 404);
+        }
+    }
+}
