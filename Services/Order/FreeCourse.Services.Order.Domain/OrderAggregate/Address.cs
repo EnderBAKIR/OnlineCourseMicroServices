281b7edd793ddd09da5d95d6f85f9efba35c9c7d
Ender Bakır <enderyx_123@hotmail.com>
2024-02-12 16:01:52 +0300

    Order , OrderItem , Address(DDD)
    The Adress class, which is a value object, was created, the Order Item, which is an entity, was created, and these two classes were defined in the Order class derived from the IaggregateRoot interface.

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -0,0 +1,39 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Address : ValueObject
+    {
+
+        public string Province { get; private set; }
+        public string Disctrict { get; private set; }
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Line { get; private set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Province;
+            yield return Disctrict;
+            yield return Street;
+            yield return ZipCode;
+            yield return Line;
+
+        }
+
+
+        public Address(string province, string disctrict, string street, string zipCode, string line)
+        {
+            Province = province;
+            Disctrict = disctrict;
+            Street = street;
+            ZipCode = zipCode;
+            Line = line;
+        }
+    }
+}
