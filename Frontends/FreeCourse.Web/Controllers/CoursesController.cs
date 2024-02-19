b3ce6c5bf3cef2e5f3d93e178c50fa27beeee8de
Ender Bakır <enderyx_123@hotmail.com>
2024-02-19 17:20:24 +0300

    CoursesController/Delete Action

--- Frontends/FreeCourse.Web/Controllers/CoursesController.cs
+++ Frontends/FreeCourse.Web/Controllers/CoursesController.cs
@@ -107,5 +107,11 @@ namespace FreeCourse.Web.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _catalogService.DeleteCourseAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
