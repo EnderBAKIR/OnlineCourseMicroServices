7148ed3153aca8392225dffe317dcecf44c92795
Ender Bakır <enderyx_123@hotmail.com>
2024-02-20 11:55:45 +0300

    IPhotoStockService / PhotoStockService / Program.cs
    IPhotoStockService Method

---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IPhotostockService.cs
@@ -0,0 +1,13 @@
+﻿using FreeCourse.Web.Models.PhotoStocks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IPhotostockService
+    {
+
+        Task<PhotoViewModel> UploadPhoto(IFormFile photo);
+
+        Task<Boolean> DeletePhoto(string photoUrl);
+
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/PhotoStockService.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Web.Models.PhotoStocks;
+using FreeCourse.Web.Services.Interfaces;
+
+namespace FreeCourse.Web.Services
+{
+    public class PhotoStockService : IPhotostockService
+    {
+        public Task<bool> DeletePhoto(string photoUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PhotoViewModel> UploadPhoto(IFormFile photo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
