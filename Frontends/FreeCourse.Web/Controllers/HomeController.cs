2e049625af6aca4cfa8af97bd5ca6deaf422a299
Ender Bakır <enderyx_123@hotmail.com>
2024-02-21 17:24:31 +0300

    UnAuthorizeException In HomeController
    If the user receives an error related to authentication, he/she is directed to the login screen.

--- Frontends/FreeCourse.Web/Controllers/HomeController.cs
+++ Frontends/FreeCourse.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using FreeCourse.Web.Models;
+﻿using FreeCourse.Web.Execption;
+using FreeCourse.Web.Models;
 using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -41,6 +43,16 @@ namespace FreeCourse.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            var errorFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            if (errorFeature!=null && errorFeature.Error is UnAuthorizeException)
+            {
+                return RedirectToAction(nameof(AuthController.LogOut), "Auth");
+            }
+
+
+
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
