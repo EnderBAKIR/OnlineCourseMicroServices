fa7c2820dcbdf8cd4a194d946db1a374a2933dab
Ender Bakır <enderyx_123@hotmail.com>
2024-02-23 19:44:48 +0300

    Added OrderController => Checkout.cshtml

---
+++ Frontends/FreeCourse.Web/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using FreeCourse.Web.Models.Orders;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IBasketService _baskerService;
+        private readonly IOrderService _orderService;
+
+        public OrderController(IBasketService baskerService, IOrderService orderService)
+        {
+            _baskerService = baskerService;
+            _orderService = orderService;
+        }
+
+        public async Task< IActionResult> Checkout()
+        {
+            var basket = await _baskerService.Get();
+
+            ViewBag.basket = basket;
+            return View(new CheckOutInfoInput());
+        }
+    }
+}
