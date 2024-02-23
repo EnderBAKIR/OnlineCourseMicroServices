33a5eefd4682929224b371c1447d7924d36ffe23
Ender Bakır <enderyx_123@hotmail.com>
2024-02-23 14:46:27 +0300

    Added IPaymentService /Was Created ServiceApiSettings Payment Path

---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using FreeCourse.Web.Models.FakePayments;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput);
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/PaymentService.cs
@@ -0,0 +1,13 @@
+﻿using FreeCourse.Web.Models.FakePayments;
+using FreeCourse.Web.Services.Interfaces;
+
+namespace FreeCourse.Web.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
