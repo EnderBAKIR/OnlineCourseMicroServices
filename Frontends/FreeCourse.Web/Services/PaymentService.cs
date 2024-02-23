4e238485fb87e5eb29c71108c67c9ef87dbb947f
Ender Bakır <enderyx_123@hotmail.com>
2024-02-23 14:52:30 +0300

    PaymentService Methods
    PaymentService's methods are completed

--- Frontends/FreeCourse.Web/Services/PaymentService.cs
+++ Frontends/FreeCourse.Web/Services/PaymentService.cs
@@ -1,13 +1,24 @@
 ﻿using FreeCourse.Web.Models.FakePayments;
 using FreeCourse.Web.Services.Interfaces;
 
+
 namespace FreeCourse.Web.Services
 {
     public class PaymentService : IPaymentService
     {
-        public Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+        private readonly HttpClient _httpClient;
+
+        public PaymentService(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+        {
+            var response = await _httpClient.PostAsJsonAsync<PaymentInfoInput>("fakepayments", paymentInfoInput);
+
+
+            return response.IsSuccessStatusCode;
         }
     }
 }
