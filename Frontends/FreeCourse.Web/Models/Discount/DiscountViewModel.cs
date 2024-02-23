033126fbf543baf0bbe5d0baf5432353e9409a77
Ender Bakır <enderyx_123@hotmail.com>
2024-02-23 08:23:29 +0300

    Added Discount View Models And Discount Validator

---
+++ Frontends/FreeCourse.Web/Models/Discount/DiscountViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeCourse.Web.Models.Discount
+{
+    public class DiscountViewModel
+    {
+        
+
+        public string UserId { get; set; }
+
+        public int Rate { get; set; }
+
+        public string Code { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Validators/DiscountApplyInputValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using FreeCourse.Web.Models.Discount;
+
+namespace FreeCourse.Web.Validators
+{
+    public class DiscountApplyInputValidator : AbstractValidator<DiscountApplyInput>
+    {
+        public DiscountApplyInputValidator()
+        {
+            RuleFor(x => x.Code).NotEmpty().WithMessage("İndirim Kodu Boş Olamaz");
+        }
+    }
+}
